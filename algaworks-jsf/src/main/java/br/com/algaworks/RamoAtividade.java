bc1f41aa3ddbc97cae656026a94082968c1b5194
Ana Cortez <anacarolcortez@hotmail.com>
2023-04-13 16:24:16 -0300

    config inicial

---
+++ algaworks-jsf/src/main/java/br/com/algaworks/RamoAtividade.java
@@ -0,0 +1,15 @@
+package br.com.algaworks;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="ramo_atividade")
+public class RamoAtividade implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+
+}
