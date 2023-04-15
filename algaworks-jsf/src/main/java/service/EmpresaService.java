425a27766c56ab1a6c06f49400b08c9baa305f1f
anacarolcortez <anacarolcortez@hotmail.com>
2023-04-15 11:37:51 -0300

    primeira fase de injecao de dependencia

---
+++ algaworks-jsf/src/main/java/service/EmpresaService.java
@@ -0,0 +1,35 @@
+package service;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import br.com.algaworks.models.Empresa;
+import br.com.algaworks.repository.Empresas;
+import util.Transacional;
+
+public class EmpresaService implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private Empresas empresas;
+	
+	@Transacional
+	public void salvar(Empresa empresa) {
+		empresas.guardar(empresa);
+	}
+	
+	@Transacional
+	public void excluir(Empresa empresa) {
+		empresas.remover(empresa);
+	}
+	
+	@Transacional
+	public List<Empresa>  listarTodas() {
+		return empresas.pesquisar("");
+	}
+	
+
+}
---
+++ algaworks-jsf/src/main/java/util/TransacionalInterceptor.java
@@ -0,0 +1,50 @@
+package util;
+
+import java.io.Serializable;
+
+import javax.inject.Inject;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+@Interceptor
+@Transacional
+public class TransacionalInterceptor implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Inject
+    private EntityManager manager;
+
+    @AroundInvoke
+    public Object invoke(InvocationContext context) throws Exception {
+        EntityTransaction trx = manager.getTransaction();
+        boolean criador = false;
+
+        try {
+            if (!trx.isActive()) {
+                trx.begin();
+                trx.rollback(); //o rollback devolve ao estado anterior e inativa a transação, evitando que o uso direto da EntityManager por outra classe (o que deve ser feito somente através da injeção de dependência), altere o banco de dados indevidamente
+
+                trx.begin();
+
+                criador = true;
+            }
+
+            return context.proceed();
+        } catch (Exception e) {
+            if (trx != null && criador) {
+                trx.rollback();
+            }
+
+            throw e;
+        } finally {
+            if (trx != null && trx.isActive() && criador) {
+                trx.commit();
+            }
+        }
+    }
+
+}
