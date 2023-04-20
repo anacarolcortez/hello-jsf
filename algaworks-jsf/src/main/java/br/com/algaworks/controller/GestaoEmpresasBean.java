df308392ad0c85b6023d36456a9d9efdda7044f9
Ana Cortez <anacarolcortez@hotmail.com>
2023-04-20 18:48:05 -0300

    edição, exclusão e importação para excel

--- algaworks-jsf/src/main/java/br/com/algaworks/controller/GestaoEmpresasBean.java
+++ algaworks-jsf/src/main/java/br/com/algaworks/controller/GestaoEmpresasBean.java
@@ -1,12 +1,15 @@
 package br.com.algaworks.controller;
 
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.faces.convert.Converter;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+
+import org.primefaces.context.RequestContext;
 
 import br.com.algaworks.models.Empresa;
 import br.com.algaworks.models.RamoAtividade;
@@ -46,23 +49,41 @@ public class GestaoEmpresasBean implements Serializable {
         empresa = new Empresa();
     }
     
+    public void prepararEdicao() {
+    	ramoAtividadeConverter = new RamoAtividadeConverter(Arrays.asList(empresa.getRamoAtividade()));
+    }
+    
     public void salvar() {
         empresaService.salvar(empresa);
         
-        if (jaHouvePesquisa()) {
-            pesquisar();
-        }
+        atualizar();
         
         messages.info("Empresa cadastrada com sucesso!");
+        
+        RequestContext.getCurrentInstance().update(Arrays.asList("empresaForm:empresasDataTable", "empresaForm:messages"));
     }
     
-    public void pesquisar() {
+	public void pesquisar() {
         listaEmpresas = empresas.pesquisar(termoPesquisa);
         
         if (listaEmpresas.isEmpty()) {
             messages.info("Sua consulta não retornou registros.");
         }
     }
+    
+    public void excluir() {
+    	empresaService.excluir(empresa);
+    	empresa = null;
+    	atualizar();
+        messages.info("Empresa excluída com sucesso!");
+    }
+    
+    private void atualizar() {
+//        if (jaHouvePesquisa()) {
+//            pesquisar();
+//        }
+    	todasEmpresas();
+	}
     
     public void todasEmpresas() {
         listaEmpresas = empresas.todas();
@@ -102,6 +123,14 @@ public class GestaoEmpresasBean implements Serializable {
     
     public Empresa getEmpresa() {
         return empresa;
+    }
+    
+    public void setEmpresa(Empresa empresa) {
+    	this.empresa = empresa;
+    }
+    
+    public boolean empresaSelecionada() {
+    	return empresa != null && empresa.getId() != null;
     }
 
 }
